576d2b46cc319c25d1f17c6c976a895feac96cfa
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-06-07 14:58:14 -0400

    Resolved bug where screens for scenes with endings wouldn't appear

--- iOS/NodeView.swift
+++ iOS/NodeView.swift
@@ -9,109 +9,91 @@ import SwiftUI
 //import ICS4U_Culminating
 
 struct NodeView: View {
-   
+    
     // MARK: Stored properties
-        let node: Node
-        @Binding var activeNode: Int
-        @State private var reader: ScrollViewProxy?
+    let node: Node
+    @Binding var activeNode: Int
+    @State private var reader: ScrollViewProxy?
+    
+    var body: some View {
         
-        var body: some View {
+        ScrollView {
             
-            ScrollView {
+            ScrollViewReader {
+                scrollViewProxy in
                 
-                ScrollViewReader {
-                    scrollViewProxy in
+                Text("")
+                    .id("top-of-page")
+                    .tag("top-of-page")
+                
+                
+                VStack(alignment: .leading) {
                     
-                    Text("")
-                        .id("top-of-page")
-                        .tag("top-of-page")
+                    // A normal story page
+                    // Page number
+                    Text("\(node.id)")
+                        .padding()
+                        .font(.custom("Georgia", size: 30, relativeTo: .headline))
                     
-
-                    VStack(alignment: .leading) {
+                    // Iterate over all the paragraphs
+                    ForEach(node.paragraphs, id: \.self) { currentParagraph in
+                        Text(currentParagraph)
+                            .padding()
+                            .font(.custom("Georgia", size: 20, relativeTo: .headline))
+                    }
+                    
+                    // Show the image, if there is one
+                    
+                    if let image = node.image {
                         
-                        // Ending page
-                        if node.ending != nil {
+                        Image(image)
+                            .resizable()
+                            .scaledToFit()
+                        
+                    }
+                    
+                    // Show choices, when they exist
+                    ForEach(node.edges, id: \.self) { currentEdge in
+                        HStack {
+                            Spacer()
                             
-                            // Returns to the first page of the book
-                            Button("restart") {
-                                
-                                activeNode = 1
-                                
-                            }
-                            
-                            // Leads to the AchievementsView
-                            AchievementsView()
-                            
-                            
-                        } else {
-                            
-                            // A normal story page
-                            // Page number
-                            Text("\(node.id)")
+                            Text(currentEdge.prompt)
+                                .italic()
                                 .padding()
-                                .font(.custom("Georgia", size: 30, relativeTo: .headline))
-                            
-                            // Iterate over all the paragraphs
-                            ForEach(node.paragraphs, id: \.self) { currentParagraph in
-                                Text(currentParagraph)
-                                    .padding()
-                                    .font(.custom("Georgia", size: 20, relativeTo: .headline))
-                            }
-                            
-                            // Show the image, if there is one
-                            
-                            if let image = node.image {
-                                
-                                Image(image)
-                                    .resizable()
-                                    .scaledToFit()
-
-                            }
-                            
-                            // Show choices, when they exist
-                            ForEach(node.edges, id: \.self) { currentEdge in
-                                HStack {
-                                    Spacer()
+                                .font(.custom("Georgia", size: 20, relativeTo: .headline))
+                                .multilineTextAlignment(.trailing)
+                                .onTapGesture {
+                                    if node.ending != nil {
+                                        
+                                        addEndingReached(currentNode: activeNode)
+                                    }
+                                    // Advance to whatever node this prompt is for
+                                    activeNode = currentEdge.destinationId
                                     
-                                    Text(currentEdge.prompt)
-                                        .italic()
-                                        .padding()
-                                        .font(.custom("Georgia", size: 20, relativeTo: .headline))
-                                        .multilineTextAlignment(.trailing)
-                                        .onTapGesture {
-                                            if node.ending != nil {
-                                                
-                                                addEndingReached(currentNode: activeNode)
-                                                
-                                            }
-                                            // Advance to whatever node this prompt is for
-                                            activeNode = currentEdge.destinationId
-                                            
-                                            // Sets the scroll view to return to the top after moving to a new page
-                                            reader?.scrollTo("top-of-page")
-
-                                        }
+                                    // Sets the scroll view to return to the top after moving to a new page
+                                    reader?.scrollTo("top-of-page")
                                     
                                 }
-                            }
                             
                         }
-                       
                     }
-                    .onAppear {
-                        self.reader = scrollViewProxy
-                    }
-
+                    
                 }
-                                
+                
+                .onAppear {
+                    self.reader = scrollViewProxy
+                }
+                
             }
-            .background(
-                Image("Background")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all))
             
         }
+        .background(
+            Image("Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all))
+        
     }
+}
 
 struct NodeView_Previews: PreviewProvider {
     static var previews: some View {
